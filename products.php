97cc00568150f0518d305e6887a6812de01051a7
quentin <q.maurice@hotmail.fr>
2014-11-14 14:32:13 +0000

    store products fully implemented

--- products.php
+++ products.php
@@ -13,10 +13,31 @@ $response = $bdd->query('SELECT * FROM products');
 
 while ($data = $response->fetch())
 {
-	echo '<div class="panel panel-default products item ' . $data['filters'] . '">';
-	echo '<div class="panel-body"><image src="image/product/'. $data['logo_file'] . '.png"><div>' . $data['description_short'] .'</div></div>';
-	echo '<div class="panel-footer">' . $data['title'] . '</div>';
-	echo '</div>';  
+?>	
+    <div class="panel item panel-default products <?php echo $data['filters'] ?>">
+      <div class='main'>
+        <div class="panel-body">
+            <image src="image/product/<?php echo $data['logo_file'] ?>.png">
+            <div><?php echo $data['description_short'] ?></div>
+          </div>
+          <div class="panel-footer">
+            <?php echo $data['title'] ?>
+          </div>
+        </div>
+        <div class='slide'> 
+          <div class="panel-body">
+            <div class='row'>
+              <div class='col-sm-8 title'><?php echo $data['title'] ?></div>
+              <div class='col-sm-4 logo_small'>
+                <image class='small' src="image/product/<?php echo $data['logo_file'] ?>.png">
+              </div>
+            </div>      
+            <div class="description_long row"><?php echo $data['description_long'] ?></div>
+            <div class="row buy"><div class='btn btn-sm'>From £1000</div></div>
+          </div>
+    	</div>
+    </div>	
+ <?php       
 }
 
 $response->closeCursor();
--- store.php
+++ store.php
@@ -14,32 +14,51 @@
 
   <?php include "header.html" ?>
 
-	<div class="container store">
+	<div class="container-fluid store">
 		<div class="col-md-2 leftslider">
 			<input type="text" placeholder="search">
 			<div class="filters">
 				<div class="filter active one" data-filter="*">Apps</div>
 						<div class="filter two" data-filter=".bestsell">Best Selling</div>
-						<div class="two">By Provider</div>
+						<div class="two nohover">By Provider</div>
 								<div class="filter three" data-filter=".zappi">Zappistore</div>
 								<div class="filter three" data-filter=":not(.zappi)">Partners</div>
 								<div class="filter three" data-filter=".mb">MB</div>
 								<div class="filter three" data-filter=".mmr">MMR</div>
                 <div class="filter three" data-filter=".tonic">Tonic</div>
-						<div class="two">By Product Tested</div>
+						<div class="two nohover">By Product Tested</div>
 								<div class="filter three" data-filter=".video">Video Ad</div>
 								<div class="filter three" data-filter=".static">Static Ad</div>
 								<div class="filter three" data-filter=".pack">Package</div>
 								<div class="filter three" data-filter=".newprod">New product</div>
 			</div>		
-		</div>	
+		</div>
 
-		<div id="container" class="col-md-10">
-      <?php include 'products.php'; ?>
+    <div id="container" class="col-md-10">	
+      <?php include 'products.php' ?>
 	  </div>	
-    
+
   </div>
     
+  <script type="text/javascript">
+    $('.main').hover(function() {
+      var test = $(this);
+      setTimeout(function(){
+        if (test.is(':hover')){
+          test.slideUp(200);
+        }
+      },200);    
+    },function(){
+      if (! $(this).next().is(':hover')){
+        $(this).slideDown(100);
+      } 
+    });
+    $('.slide').mouseleave(function() {
+         $(this).prev().slideDown(100);
+    });
+  </script>  
+
+
   <script type="text/javascript">
   	var $container = $('#container');
   	// init
@@ -50,10 +69,10 @@
   	});
 	
   	$('.filter').on( 'click', function() {
-  	var filterValue = $(this).attr('data-filter');
-  	$container.isotope({ filter: filterValue });
-  	$('.active').removeClass('active');
-  	$(this).addClass('active');
+  	 var filterValue = $(this).attr('data-filter');
+  	 $container.isotope({ filter: filterValue });
+  	 $('.active').removeClass('active');
+  	 $(this).addClass('active');
 		});
   </script>
 
