599c9f2271b9f0d0fa2a97a6a4c57378c4050e31
quentin <q.maurice@hotmail.fr>
2014-12-08 17:59:40 +0000

    almost finish product page

--- impackt_product_conf.php
+++ impackt_product_conf.php
@@ -9,6 +9,7 @@
 	<link href="css/slider.css" rel="stylesheet">
 	<script src="js/jquery.js"></script>
 	<script src ="js/bootstrap.js"></script>
+	<script src="js/html5.image.preview.js"></script>
 </head>
 
 <body>
@@ -18,13 +19,17 @@
 		<div class="left-help col-md-3">
 			<div class="left-help-wrapper">
 				<div class="left-help-header row">
-					<div class="help-title active" question="0">Select your number of executions</div>
-					<div class="help-title" question="1">Select your sample size</div>
-					<div class="help-title" question="2">Select your country</div>
-					<div class="help-title" question="3">Select your region</div>
-					<div class="help-title" question="4">Select your target group</div>
-					<div class="help-title" question="5">Select your age range</div>
-					<div class="help-title" question="6">Select your gender</div>
+					<div class="help-title active" question="0">Add and upload your creatives</div>
+					<div class="help-title" question="1">Select your pack name</div>
+					<div class="help-title" question="2">Select your brand namey</div>
+					<div class="help-title" question="3">Select your product name</div>
+					<div class="help-title" question="4">Select your category</div>
+					<div class="help-title" question="5">Select your pack range</div>
+					<div class="help-title" question="6">Select your benchmark pack</div>
+					<div class="help-title" question="7">Select your category for norms</div>
+					<div class="help-title" question="8">Select your pack diagnostic</div>
+					<div class="help-title" question="9">Select your pack expectations</div>
+					<div class="help-title" question="10">Select your pack format</div>
 				</div>
 				<div class="help-wrapper">
 					<div class="help-content">
@@ -78,7 +83,7 @@
 					</div>
 				</div>
 			</div>	
-			<div class="selection active product-select" question="0">
+			<div class="selection active" question="0">
 				<div class="selection-title">Add and upload up to 5 creatives</div>
 				<div class="selector-wrapper">
 					<div class="selector active"></div>
@@ -87,6 +92,8 @@
 					<div class="form-group group">
 						<div class="square upload added" cell="1">
 							<span class="glyphicon glyphicon-open icon-center"></span>
+							<input type="file" name="imagefile" onchange="previewImage(this,[120],4);">
+							<div class="imagePreview"></div>
 						</div>
 						<div class="square upload" cell="2">
 							<span class="glyphicon glyphicon-plus icon-center"></span>
@@ -117,10 +124,10 @@
 				</div>
 				<div class="user-select input-group-sm five group">
 					<input name="textinput" type="text" placeholder="ex: Red Pack" class="form-control">
-					<input name="textinput" type="text" placeholder="ex: Yellow Pack" class="form-control " disabled>
-					<input name="textinput" type="text" placeholder="ex: Green Pack" class="form-control " disabled>
-					<input name="textinput" type="text" placeholder="ex: Blue Pack" class="form-control " disabled>
-					<input name="textinput" type="text" placeholder="ex: Purple Pack" class="form-control " disabled>
+					<input name="textinput" type="text" placeholder="ex: Yellow Pack" class="form-control pack2">
+					<input name="textinput" type="text" placeholder="ex: Green Pack" class="form-control pack3">
+					<input name="textinput" type="text" placeholder="ex: Blue Pack" class="form-control pack4">
+					<input name="textinput" type="text" placeholder="ex: Purple Pack" class="form-control pack5">
 				</div>
 			</div>
 			<div class="selection product-conf" question="2">
@@ -130,10 +137,10 @@
 				</div>
 				<div class="user-select input-group-sm five group">
 					<input name="textinput" type="text" placeholder="ex: Cadbury" class="form-control">
-					<input name="textinput" type="text" placeholder="ex: Cadbury" class="form-control " disabled>
-					<input name="textinput" type="text" placeholder="ex: Cadbury" class="form-control " disabled>
-					<input name="textinput" type="text" placeholder="ex: Cadbury" class="form-control " disabled>
-					<input name="textinput" type="text" placeholder="ex: Cadbury" class="form-control " disabled>
+					<input name="textinput" type="text" placeholder="ex: Cadbury" class="form-control pack2">
+					<input name="textinput" type="text" placeholder="ex: Cadbury" class="form-control pack3">
+					<input name="textinput" type="text" placeholder="ex: Cadbury" class="form-control pack4">
+					<input name="textinput" type="text" placeholder="ex: Cadbury" class="form-control pack5">
 				</div>
 			</div>
 			<div class="selection product-conf" question="3">
@@ -143,10 +150,10 @@
 				</div>
 				<div class="user-select input-group-sm five group">
 					<input name="textinput" type="text" placeholder="ex: Cadbury Daily Milk" class="form-control">
-					<input name="textinput" type="text" placeholder="ex: Cadbury Daily Milk" class="form-control " disabled>
-					<input name="textinput" type="text" placeholder="ex: Cadbury Daily Milk" class="form-control " disabled>
-					<input name="textinput" type="text" placeholder="ex: Cadbury Daily Milk" class="form-control " disabled>
-					<input name="textinput" type="text" placeholder="ex: Cadbury Daily Milk" class="form-control " disabled>
+					<input name="textinput" type="text" placeholder="ex: Cadbury Daily Milk" class="form-control pack2">
+					<input name="textinput" type="text" placeholder="ex: Cadbury Daily Milk" class="form-control pack3">
+					<input name="textinput" type="text" placeholder="ex: Cadbury Daily Milk" class="form-control pack4">
+					<input name="textinput" type="text" placeholder="ex: Cadbury Daily Milk" class="form-control pack5">
 				</div>
 			</div>
 			<div class="selection product-conf" question="4">
@@ -156,10 +163,10 @@
 				</div>
 				<div class="user-select input-group-sm five group">
 					<input name="textinput" type="text" placeholder="ex: Chocolate" class="form-control">
-					<input name="textinput" type="text" placeholder="ex: Chocolate" class="form-control" disabled>
-					<input name="textinput" type="text" placeholder="ex: Chocolate" class="form-control" disabled>
-					<input name="textinput" type="text" placeholder="ex: Chocolate" class="form-control" disabled>
-					<input name="textinput" type="text" placeholder="ex: Chocolate" class="form-control" disabled>
+					<input name="textinput" type="text" placeholder="ex: Chocolate" class="form-control pack2">
+					<input name="textinput" type="text" placeholder="ex: Chocolate" class="form-control pack3">
+					<input name="textinput" type="text" placeholder="ex: Chocolate" class="form-control pack4">
+					<input name="textinput" type="text" placeholder="ex: Chocolate" class="form-control pack5">
 				</div>
 			</div>
 			<div class="selection product-conf" question="5">
@@ -170,7 +177,7 @@
 				<div class="user-select five group">
 					<div class="controls yesno">					   
 					   	<label class="radio-inline" for="radios-0">
-    						<input type="radio" name="range-cell1" id="radios-10" value="Yes">
+    						<input type="radio" name="range-cell1" id="radios-0" value="Yes">
     						Yes
     					</label>
     					<label class="radio-inline" for="radios-1">
@@ -178,43 +185,43 @@
     						No
     					</label>
     				</div>
-    				<div class="controls yesno">					   
+    				<div class="controls yesno pack2">					   
 					   	<label class="radio-inline" for="radios-2">
-    						<input type="radio" name="range-cell2" id="radios-2" value="Yes" disabled>
+    						<input type="radio" name="range-cell2" id="radios-2" value="Yes">
     						Yes
     					</label>
     					<label class="radio-inline" for="radios-3">
-    						<input type="radio" name="range-cell2" id="radios-3" value="No" disabled>
+    						<input type="radio" name="range-cell2" id="radios-3" value="No">
     						No
     					</label>
     				</div>
-    				<div class="controls yesno">					   
+    				<div class="controls yesno pack3">					   
 					   	<label class="radio-inline" for="radios-4">
-    						<input type="radio" name="range-cell3" id="radios-4" value="Yes" disabled>
+    						<input type="radio" name="range-cell3" id="radios-4" value="Yes">
     						Yes
     					</label>
     					<label class="radio-inline" for="radios-5">
-    						<input type="radio" name="range-cell3" id="radios-5" value="No" disabled>
+    						<input type="radio" name="range-cell3" id="radios-5" value="No">
     						No
     					</label>
     				</div>
-    				<div class="controls yesno">					   
+    				<div class="controls yesno pack4">					   
 					   	<label class="radio-inline" for="radios-6">
-    						<input type="radio" name="range-cell4" id="radios-6" value="Yes" disabled>
+    						<input type="radio" name="range-cell4" id="radios-6" value="Yes">
     						Yes
     					</label>
     					<label class="radio-inline" for="radios-7">
-    						<input type="radio" name="range-cell4" id="radios-7" value="No" disabled>
+    						<input type="radio" name="range-cell4" id="radios-7" value="No">
     						No
     					</label>
     				</div>
-    				<div class="controls yesno">					   
+    				<div class="controls yesno pack5">					   
 					   	<label class="radio-inline" for="radios-8">
-    						<input type="radio" name="range-cell5" id="radios-8" value="Yes" disabled>
+    						<input type="radio" name="range-cell5" id="radios-8" value="Yes">
     						Yes
     					</label>
     					<label class="radio-inline" for="radios-9">
-    						<input type="radio" name="range-cell5" id="radios-9" value="No" disabled>
+    						<input type="radio" name="range-cell5" id="radios-9" value="No">
     						No
     					</label>
     				</div>	
@@ -226,23 +233,152 @@
 					<div class="selector"></div>
 				</div>
 				<div class="user-select five group">					   
-   					<input type="radio" name="benchmark" id="radios-0" value="1">
-   					<input type="radio" name="benchmark" id="radios-0" value="2">
-   					<input type="radio" name="benchmark" id="radios-0" value="3">
-   					<input type="radio" name="benchmark" id="radios-0" value="4">
-   					<input type="radio" name="benchmark" id="radios-0" value="5">
+   					<input type="radio" name="benchmark" id="radios-0" value="1" checked>
+   					<input type="radio" name="benchmark" id="radios-0" value="2" class="pack2">
+   					<input type="radio" name="benchmark" id="radios-0" value="3" class="pack3">
+   					<input type="radio" name="benchmark" id="radios-0" value="4" class="pack4">
+   					<input type="radio" name="benchmark" id="radios-0" value="5" class="pack5">
    				</div>	
 			</div>
 			<div class="selection product-conf-top" question="7">
 				<div class="product-conf-header">
 					<div class="product-conf-header-center">Project Configuration</div>
 				</div>
-				<div class="selection-title" >Pack Name</div>
+				<div class="selection-title" >Category (Norms)</div>
 				<div class="selector-wrapper">
 					<div class="selector"></div>
 				</div>
+				<div class="user-select">
+					<div class="form-group group">
+						<div class="col-md-5">
+					    	<select id="target-group" name="category" class="form-control">
+					    	  <option value="1">Drink</option>
+					    	  <option value="2">Food</option>
+					    	  <option value="3">Car</option>
+					    	  <option value="4">No idea</option>
+					    	</select>
+					    </div>
+					</div>
+				</div>
 			</div>	
-			<div class="next-step col-md-3 pull-right">
+			<div class="selection" question="8">
+				<div class="selection-title">Pack Diagnostics</div>
+				<div class="selector-wrapper">
+					<div class="selector"></div>
+				</div>
+				<div class="user-select five group">					   
+   					<input name="textinput" type="text" placeholder="ex: Color" class="form-control">
+					<input name="textinput" type="text" placeholder="ex: Logo" class="form-control">
+					<input name="textinput" type="text" placeholder="ex: Tagline" class="form-control">
+   				</div>	
+			</div>
+			<div class="selection" id="expectations" question="9">
+				<div class="selection-title">Pack Expectations</div>
+				<div class="selector-wrapper">
+					<div class="selector"></div>
+				</div>
+				<div class="user-select four group">					   
+   					<input name="textinput" type="text" placeholder="ex: will really fill me up" class="form-control">
+					<input name="textinput" type="text" placeholder="ex: will be good tasting" class="form-control">
+					<input name="textinput" type="text" placeholder="ex: will give me power" class="form-control">
+					<input name="textinput" type="text" placeholder="ex: won't do anything" class="form-control">
+					<span class="glypgicon glyphicon-plus addline"></span>
+   				</div>
+   				<div class="user-select four group addedline">					   
+   					<input name="textinput" type="text" class="form-control">
+					<input name="textinput" type="text" class="form-control">
+					<input name="textinput" type="text" class="form-control">
+					<input name="textinput" type="text" class="form-control">
+					<span class="glypgicon glyphicon-plus addline"></span>
+   				</div>
+   				<div class="user-select four group addedline">					   
+   					<input name="textinput" type="text" class="form-control">
+					<input name="textinput" type="text" class="form-control">
+					<input name="textinput" type="text" class="form-control">
+					<input name="textinput" type="text" class="form-control">
+   				</div>	
+			</div>
+			<div class="selection" id="expectations" question="10">
+				<div class="selection-title">Pack Format</div>
+				<div class="selector-wrapper">
+					<div class="selector"></div>
+				</div>
+				<div class="user-select">
+					<div class="form-group group">
+						<div class="pack-format">
+							<label for="format-0">
+								<img src="image/pack_format/rectangular_p.png">
+								<div class="caption">
+									<p>Rectangular Portrait</p>
+									<a>view images</a>
+									<input type="radio" name="format" id="format-0" value="1" checked>
+								</div>
+							</label>
+						</div>
+						<div class="pack-format">
+							<label for="format-1">
+								<img src="image/pack_format/rectangular_l.png">
+								<div class="caption">
+									<p>Rectangular Landscape</p>
+									<a>view images</a>
+									<input type="radio" name="format" id="format-1" value="1" checked>
+								</div>
+							</label>
+						</div>
+						<div class="pack-format">
+							<label for="format-2">
+								<img src="image/pack_format/rectangular_t.png">
+								<div class="caption">
+									<p>Rectangular Thin</p>
+									<a>view images</a>
+									<input type="radio" name="format" id="format-2" value="1" checked>
+								</div>
+							</label>
+						</div>
+						<div class="pack-format">
+							<label for="format-3">
+								<img src="image/pack_format/rounded.png">
+								<div class="caption">
+									<p>Rounded Bottles</p>
+									<a>view images</a>
+									<input type="radio" name="format" id="format-3" value="1" checked>
+								</div>
+							</label>
+						</div>
+						<div class="pack-format">
+							<label for="format-4">
+								<img src="image/pack_format/angular.png">
+								<div class="caption">
+									<p>Angular Bottles</p>
+									<a>view images</a>
+									<input type="radio" name="format" id="format-4" value="1" checked>
+								</div>
+							</label>
+						</div>
+						<div class="pack-format">
+							<label for="format-5">
+								<img src="image/pack_format/cans.png">
+								<div class="caption">
+									<p>Can and Jars</p>
+									<a>view images</a>
+									<input type="radio" name="format" id="format-5" value="1" checked>
+								</div>
+							</label>
+						</div>
+						<div class="pack-format">
+							<label for="format-6">
+								<img src="image/pack_format/tubs.png">
+								<div class="caption">
+									<p>Tubs and others</p>
+									<a>view images</a>
+									<input type="radio" name="format" id="format-6" value="1" checked>
+								</div>
+							</label>
+						</div>
+					</div>	
+				</div>
+			</div>	
+			<div class="next-step col-md-1 pull-right">
 				<a href="#" class="btn btn-primary"><span class="glyphicon glyphicon-forward"></span> Next Step</a>
 			</div>	
 		</div>
@@ -254,10 +390,6 @@
 	<script src="js/header.js"></script>
 	<script data-cfasync="false" type='text/javascript' src="js/olark.js"></script>
 	<script>
-		$("#sample-size").slider({
-			tooltip: 'always'
-		});
-
 		scroll_follow = function(item) {
 	        scroll = $(document).scrollTop();
 	        offset = $('.sample-selection').offset().top;
@@ -293,7 +425,7 @@
 		}
 
 		$(window).load(function() {
-			var $el, topPos, newHeight;
+			var $el, topPos, newHeight, magicmove;
 
 		   	$(".main-selection").append("<div id='magic-line'></div>");
 		    
@@ -305,31 +437,49 @@
 		        .css("top", $(".main-selection .selection.active").position().top)
 		        .data("origTop", $(".main-selection .active").position().top)
 		        .data("origHeight", $magicLine.outerHeight());
-		                
-		    $(".main-selection .selection").mousedown(function() {
-		        $el = $(this);
-		        var num = parseInt($el.attr('question'),10);
-				if($el.hasClass('active')){return;}
-		        topPos = $el.position().top;
-		        newHeight = $el.outerHeight();
+
+		    magicmove = function(el){
+		    	var num = parseInt(el.attr('question'),10);
+		        topPos = el.position().top;
+		        newHeight = el.outerHeight();
 		        $('.selection.active').css({'color':'black'});
 		        $magicLine.stop().animate({
 		            top: topPos,
-		            height: newHeight,
+		            height: newHeight
 		        },function(){
 				changeactive(num);
 		   		 });
-		    });	    
+		    }    
+		                
+		    $(".main-selection .selection").mousedown(function() {
+		        $el = $(this);
+		        if($el.hasClass('active')){return;}
+		        magicmove($el);
+		    });
+
+		    $('.addline').mousedown(function(){
+				$(this).parent().next().show();
+				magicmove($('#expectations'));
+				$(this).hide();
+			});	    
 		});
 	
-	$('.square:not(.added)>.icon-center').click(function(){
-		$(this).parent().addClass('added');
-		$(this).removeClass('glyphicon-plus').addClass('glyphicon-open');
+	$('.square:not(.added)').mousedown(function(){
+		var cell = $(this).attr('cell');
+		$(this).addClass('added');
+		$(this).find('.icon-center').removeClass('glyphicon-plus').addClass('glyphicon-open');
+		$('.pack'+cell).each(function(){
+			$(this).show();
+		});
 	});
 
 	$('.trash').mouseup(function(){
-		$(this).parent().find('.icon-center').removeClass('glyphicon-open').addClass('glyphicon-plus');
+		var cell = $(this).parent().attr('cell');
+		$(this).prev().removeClass('glyphicon-open').addClass('glyphicon-plus');
 		$(this).parent().removeClass('added');
+		$('.pack'+cell).each(function(){
+			$(this).hide();
+		});
 	});
 
 	</script>
